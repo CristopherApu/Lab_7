c17f1f2440f033b84169bb3d82450bea8637f46c
Cristopher <74779158+CristopherApu@users.noreply.github.com>
2020-12-21 18:21:20 -0300

    Add files via upload

---
+++ Actividad2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	
+	int X;
+	int Y;
+	
+	printf("Por favor, ingrese el primer numero\n");
+	scanf("%d", &X);
+	printf("Por favor, ingrese el segundo numero\n");
+	scanf("%d", &Y);
+	
+	/* quiero que X se sume por si mismo Y veces*/ 
+	
+	for(int i = 0 ; Y ; i + X){
+		printf("%d\n", i);
+		}	
+	
+return 0;
+}
